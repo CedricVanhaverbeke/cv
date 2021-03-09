22d7f49c148ba17f8b599d12c13114ad9cb5eb51
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2021-03-09 10:38:07 +0100

    add some margin

--- src/components/SecondaryHeader.js
+++ src/components/SecondaryHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SecondaryHeader = ({ children }) => {
-  return <h2 className="italic text-center my-4">{children}</h2>;
+  return <h2 className="italic text-center my-4 mx-10">{children}</h2>;
 };
 
 export default SecondaryHeader;
